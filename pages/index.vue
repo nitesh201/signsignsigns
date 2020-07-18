1a7b89c2fea25dd2ada4f143bab9b275d3bc1c24
Nitesh Menon <nitesh.menon@kitware.com>
2020-07-18 16:45:47 -0400

    removed mockup; changed directory structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,45 @@
 <template>
   <div id="app">
-    <Home/>
+    <div>
+      <Header />
+      <div id="content">
+          <GalleryView
+              :galleryItems="signsPosts"
+              @itemSelected="onItemSelected"
+          />
+      </div>
+    </div>
   </div>
 </template>
 
+
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
-import Home from '../components/Home.vue';
+import Vue from 'vue'
+import { defineComponent, ref, computed } from '@vue/composition-api';
+import Header from '@/components/Header.vue';
+import GalleryView from '@/components/GalleryView.vue';
+import ImageView from '@/components/ImageView.vue';
 
 export default defineComponent({
-  name: 'App',
   components: {
-    Home,
+    Header,
+    GalleryView,
+    ImageView,
   },
-  head() {
+  setup(props, context) {
+    const forewordText = 'We are a collective of artists and designers working both to revive past '
+    + 'political graphics and to encourage new visual responses to current events. Every graphic is '
+    + 'available as a free print-ready download for use in political protests or mailing campaigns.';
+    const signsPosts = computed(() => context.root.$store.state.signsPosts)
+    const onItemSelected = (index: number) => {
+      console.log(signsPosts.value[index])
+      context.root.$router.push(signsPosts.value[index].slug)
+    }
     return {
-      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+      signsPosts,
+      forewordText,
+      onItemSelected
     };
-  },
-  setup() {
-    return {};
   },
 });
 </script>
@@ -35,4 +55,11 @@ export default defineComponent({
   padding-right: 40px;
   padding-left: 40px;
 }
+</style>
+
+<style scoped>
+/* #content {
+    height: calc(100vh - 200px);
+} */
+
 </style>
